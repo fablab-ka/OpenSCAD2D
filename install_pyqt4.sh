2f662a781e467098552f50685b3c95a0a3340059
Christian J <c.juelg@gmail.com>
2015-03-08 00:18:18 +0100

    add #!/bin/sh

--- install_pyqt4.sh
+++ install_pyqt4.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 curl -L -O http://sourceforge.net/projects/pyqt/files/sip/sip-4.16.5/sip-4.16.5.tar.gz
 tar -xvf sip-4.16.5.tar.gz
 cd sip-4.16.5
@@ -5,6 +6,7 @@ python configure.py
 make -j 4
 sudo make install
 cd ..
+
 curl -L -O http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt-x11-gpl-4.11.3.tar.gz
 tar -xvf PyQt-x11-gpl-4.11.3.tar.gz
 cd PyQt-x11-gpl-4.11.3
